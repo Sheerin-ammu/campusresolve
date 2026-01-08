83c0dbd86c44dca67670003c20418eaf9164890b
Sheerin-ammu <sheerinhasan673@gmail.com>
2026-01-08 17:25:49 +0530

    Fix homepage redirect

--- campusresolve/urls.py
+++ campusresolve/urls.py
@@ -16,11 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.auth import views as auth_views
+from django.shortcuts import redirect
+
+def home_redirect(request):
+    return redirect('login')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_redirect),            # 🔥 THIS FIXES 404
     path('', include('complaints.urls')),
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
+
